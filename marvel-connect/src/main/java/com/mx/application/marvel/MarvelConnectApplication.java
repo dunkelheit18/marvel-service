e6e8d3838075bc4abdffdaa0ba9a4d26ca556243
Jose Aaron Hernandez Gonzalez <aaron.hg@outlook.com>
2023-11-21 14:48:48 -0600

    feat: Creacion del proyecto.
    Construccion del proyecto:
    
    -> Proyecto para el consumo de la API de Marvel.

---
+++ marvel-connect/src/main/java/com/mx/application/marvel/MarvelConnectApplication.java
@@ -0,0 +1,13 @@
+package com.mx.application.marvel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarvelConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarvelConnectApplication.class, args);
+	}
+
+}
