54e3ddc9d807119b954bb1abd955d7c0ad6bf5fd
Jose Aaron Hernandez Gonzalez <aaron.hg@outlook.com>
2023-11-21 19:20:50 -0600

    feat: Creacion del proyecto API Rest.
    Esta api hara el uso de la aplicacion que consume la API de Marvel.

---
+++ marvel-api-rest/src/main/java/com/mx/application/marvel/api/MarvelApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.mx.application.marvel.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarvelApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarvelApiRestApplication.class, args);
+	}
+
+}
---
+++ marvel-api-rest/src/test/java/com/mx/application/marvel/api/MarvelApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mx.application.marvel.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarvelApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
