6d0089f72277401ca3e14a8c3ce7b3af1db52b0e
Jose Aaron Hernandez Gonzalez <aaron.hg@outlook.com>
2023-11-24 02:01:00 -0600

    refactor: refactorizacion del request del controller y el proceso de guardado.
    -> Se adecua el retorno del request en el controller
    -> Se adecua el proceso de guardado.

--- marvel-api-rest/src/main/java/com/mx/application/marvel/api/application/controller/MarvelApiController.java
+++ marvel-api-rest/src/main/java/com/mx/application/marvel/api/application/controller/MarvelApiController.java
@@ -4,9 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import com.mx.application.marvel.api.business.service.MarvelProcessDataService;
 
@@ -20,21 +20,17 @@ public class MarvelApiController {
 	@Autowired
 	private MarvelProcessDataService marvelProcessDataService;
 	
-	@PostMapping(path = "/characters/extractName")
-	public ResponseEntity<?> extracToMarvelCharactersByName
-		(@RequestParam(name = "name", required = true) String name) {
+	@PostMapping(path = "/characters/extractName/{name}")
+	public ResponseEntity<Boolean> extracToMarvelCharactersByName
+		(@PathVariable(name = "name", required = true) String name) {
 		
-		marvelProcessDataService.saveCharactersDataByName(name);
-		
-		return new ResponseEntity<>(HttpStatus.OK);
+		return new ResponseEntity<>(marvelProcessDataService.saveCharactersDataByName(name), HttpStatus.OK);
 	}
 	
-	@PostMapping(path = "/characters/extractId")
-	public ResponseEntity<?> extracToMarvelCharactersById
-		(@RequestParam(name = "id", required = true) Integer id) {
+	@PostMapping(path = "/characters/extractId/{id}")
+	public ResponseEntity<Boolean> extracToMarvelCharactersById
+		(@PathVariable(name = "id", required = true) Integer id) {
 		
-		marvelProcessDataService.saveCharactersDataById(id);
-		
-		return new ResponseEntity<>(HttpStatus.OK);
+		return new ResponseEntity<>(marvelProcessDataService.saveCharactersDataById(id), HttpStatus.OK);
 	}
 }
--- marvel-api-rest/src/main/java/com/mx/application/marvel/api/business/service/impl/MarvelProcessDataServiceImpl.java
+++ marvel-api-rest/src/main/java/com/mx/application/marvel/api/business/service/impl/MarvelProcessDataServiceImpl.java
@@ -5,6 +5,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 import org.springframework.util.ObjectUtils;
 
 import com.mx.application.marvel.api.business.service.MarvelExctractService;
@@ -22,7 +23,6 @@ import com.mx.application.marvel.api.persistence.repository.MarvelStoriesReposit
 import com.mx.application.marvel.service.model.MarvelCharacteresResponse;
 import com.mx.application.marvel.service.model.MarvelComics;
 import com.mx.application.marvel.service.model.MarvelEvents;
-import com.mx.application.marvel.service.model.MarvelItems;
 import com.mx.application.marvel.service.model.MarvelSeries;
 import com.mx.application.marvel.service.model.MarvelStories;
 
@@ -96,115 +96,99 @@ public class MarvelProcessDataServiceImpl implements MarvelProcessDataService{
 	}
 	
 	private void saveComics(List<MarvelComics> lisComics, Integer idCharacters) {
-		
-		List<MarvelItems> items = null;
-		Comics comics = null;
-		for (MarvelComics marvelComics : lisComics) {
-			items = marvelComics.getItems();
-			
-			if(items != null && !items.isEmpty()) {
-				for (MarvelItems marvelItems : items) {
-					comics = new Comics();
+	
+		if(!CollectionUtils.isEmpty(lisComics)) {
+			List<Comics> comicsList =  lisComics.stream().filter
+					(n -> !CollectionUtils.isEmpty(n.getItems())).map(i -> {		
+				return i.getItems().stream().map(mi -> {
+					Comics comics = new Comics();
 					comics.setIdCharacter(idCharacters);
-					comics.setName(marvelItems.getName());
-					comics.setResources(marvelItems.getResourceURI());
-					
-					marvelComicsRepository.save(comics);
-					marvelComicsRepository.flush();
-				}
-			}
+					comics.setName(mi.getName());
+					comics.setResources(mi.getResourceURI());				
+					return comics;
+				}).toList();
+			}).toList().get(0);
+			
+			marvelComicsRepository.saveAll(comicsList);
 		}
 	}
 	
 	private void saveEvents(List<MarvelEvents> lisevEnts, Integer idCharacters) {
-		
-		List<MarvelItems> items = null;
-		Events events = null;
-		 for (MarvelEvents marvelEvents : lisevEnts) {
-			 items = marvelEvents.getItems();
-			 if(items != null && !items.isEmpty()) {
-				 for (MarvelItems marvelItems : items) {
-					 events = new Events();
+		 
+		if(!CollectionUtils.isEmpty(lisevEnts)) {
+			List<Events> eventsList = lisevEnts.stream().filter
+					(n -> !CollectionUtils.isEmpty(n.getItems())).map(i -> {
+				return i.getItems().stream().map(e -> {
+					 Events events = new Events();
 					 events.setIdCharacter(idCharacters);
-					 events.setName(marvelItems.getName());
-					 events.setResources(marvelItems.getResourceURI());
-					 
-					 marvelEventsRepository.save(events);
-					 marvelEventsRepository.flush();
-				}
-			 }
+					 events.setName(e.getName());
+					 events.setResources(e.getResourceURI());
+					 return events;
+				 }).toList();
+			 }).toList().get(0);
+			
+			 marvelEventsRepository.saveAll(eventsList);
 		}
 	}
 	
 	private void saveSeries(List<MarvelSeries> lisevSeries, Integer idCharacters) {
 		
-		List<MarvelItems> items = null;
-		Series series = null;
-		for (MarvelSeries seriesItems : lisevSeries) {
-			 items = seriesItems.getItems();
-			
-			 if(items != null && !items.isEmpty()) {
-				 
-				 for (MarvelItems marvelItems : items) {
-					 series = new Series();
+	 if(!CollectionUtils.isEmpty(lisevSeries)) {
+		 List<Series> seriesList = lisevSeries.stream().filter
+					(n -> !CollectionUtils.isEmpty(n.getItems())).map(i -> {
+				return i.getItems().stream().map(s -> {
+					Series series = new Series();
 					 series.setIdCharacter(idCharacters);
-					 series.setName(marvelItems.getName());
-					 series.setResources(marvelItems.getResourceURI());
-					 
-					 marvelSeriesRepository.save(series);
-					 marvelSeriesRepository.flush();
-				}
-			 }
-		}
+					 series.setName(s.getName());
+					 series.setResources(s.getResourceURI());
+					 return series;
+				}).toList();
+			}).toList().get(0);
+			
+			marvelSeriesRepository.saveAll(seriesList);
+	 }
 	}
 	
 	private void saveStories(List<MarvelStories> lisStories, Integer idCharacters) {
 		
-		List<MarvelItems> items = null;
-		Stories stories = null;
-		
-		for (MarvelStories marvelStories : lisStories) {
-			
-			items = marvelStories.getItems();
-			
-			if(items != null && !items.isEmpty()) {
-			
-				for (MarvelItems marvelItems : items) {
-					stories = new Stories();
-					
+	  if(!CollectionUtils.isEmpty(lisStories)) {
+		  List<Stories> storiesList =	lisStories.stream().filter
+					(n -> !CollectionUtils.isEmpty(n.getItems())).map(i -> {
+				return i.getItems().stream().map(s -> {
+					Stories stories = new Stories();				
 					stories.setIdCharacter(idCharacters);
-					stories.setName(marvelItems.getName());
-					stories.setResources(marvelItems.getResourceURI());
-					
-					marvelStoriesRepository.save(stories);
-					marvelStoriesRepository.flush();
-				}
-			}
-		}
+					stories.setName(s.getName());
+					stories.setResources(s.getResourceURI());
+					return stories;
+				}).toList();	
+			}).collect(Collectors.toList()).get(0);
+			
+			marvelStoriesRepository.saveAll(storiesList);
+	  }
 	}
 	
 	private void saveCharacters(Integer id, String name) {
 			
 		List<Object> listObject = marvelCharactersRepository.getAllCharactersId(id);
 		
-		List<Characters> listCharac = listObject.stream().map(o -> {
+		if(!CollectionUtils.isEmpty(listObject)) {
+			List<Characters> listCharac = listObject.stream().filter(n -> n != null).map(o -> {		
+				Characters chara = new Characters();
+				Object[] obj = ObjectUtils.toObjectArray(o);
+				
+				if(obj != null && obj.length > 0) {
+					chara.setIdComics(Integer.parseInt(obj[0].toString()));
+					chara.setIdEvents(Integer.parseInt(obj[1].toString()));
+					chara.setIdSeries(Integer.parseInt(obj[2].toString()));
+					chara.setIdStories(Integer.parseInt(obj[3].toString()));
+					chara.setIdCharacter(id);
+					chara.setNameCharacter(name);
+				}
+				
+				return chara;
+			}).collect(Collectors.toList());
 			
-			Characters chara = new Characters();
-			Object[] obj = ObjectUtils.toObjectArray(o);
-			
-			chara.setIdComics(Integer.parseInt(obj[0].toString()));
-			chara.setIdEvents(Integer.parseInt(obj[1].toString()));
-			chara.setIdSeries(Integer.parseInt(obj[2].toString()));
-			chara.setIdStories(Integer.parseInt(obj[3].toString()));
-			chara.setIdCharacter(id);
-			chara.setNameCharacter(name);
-			
-			return chara;
-		}).collect(Collectors.toList());
-		
-		listCharac.stream().forEach(c -> {
-			marvelCharactersRepository.save(c);
-			marvelCharactersRepository.flush();
-		});
+			marvelCharactersRepository.saveAll(listCharac);
+		}
 	}
 }
